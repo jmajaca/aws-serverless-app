51101e94435a84d1cca1a875fa832a56828fc1e0
Josip Majača <josip.majaca@njuskalo.hr>
2024-02-01 19:49:49 +0100

    Try to fix artifact download

--- app/src/routers/hello_world.py
+++ app/src/routers/hello_world.py
@@ -10,5 +10,5 @@ router = APIRouter(prefix="/hello-world")
 
 @router.get(path="", description="Main application endpoint")
 async def hello_world() -> HelloWorldSchema:
-    logger.info("Received request for hello world")
+    logger.info("Received request for hello world.")
     return HelloWorldSchema(message="hello world", timestamp=datetime.now())
